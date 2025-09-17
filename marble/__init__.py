af8f68b7523f52fda1d994dc921526e37ac6bd70
FreddyE1982 <frederik.egger@gmail.com>
2025-09-17 13:52:52 +0200

    Add learnable toggles and resource allocator metrics

--- marble/__init__.py
+++ marble/__init__.py
@@ -6,7 +6,12 @@ except Exception:
     pass
 
 from .auto_param import enable_auto_param_learning
-from .learnables_yaml import updatelearnablesyaml
+from .learnables_yaml import (
+    learnableIsOn,
+    learnableOFF,
+    learnableON,
+    updatelearnablesyaml,
+)
 from .plugin_encoder import PluginEncoder
 from .action_sampler import compute_logits, sample_actions, select_plugins
 from .offpolicy import Trajectory, importance_weights, doubly_robust
@@ -27,4 +32,7 @@ __all__ = [
     "BUDGET_LIMIT",
     "snapshot_to_image",
     "updatelearnablesyaml",
+    "learnableON",
+    "learnableOFF",
+    "learnableIsOn",
 ]
