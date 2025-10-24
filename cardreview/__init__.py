79a2649fbf4eeaa985a80cf6f3342dfe78afed05
FreddyE1982 <frederik.egger@gmail.com>
2025-10-24 06:44:45 +0200

    Add card design toolkit for CardReview app

---
+++ cardreview/__init__.py
@@ -0,0 +1,29 @@
+"""CardReview card design toolkit.
+
+This package contains helper classes that support creating richly formatted
+flash cards for the CardReview application.
+"""
+
+from .design import (
+    CardDimensions,
+    ColorStyle,
+    CardPalette,
+    CardFont,
+    CardElement,
+    TextElement,
+    ImageElement,
+    CardFace,
+    CardDesign,
+)
+
+__all__ = [
+    "CardDimensions",
+    "ColorStyle",
+    "CardPalette",
+    "CardFont",
+    "CardElement",
+    "TextElement",
+    "ImageElement",
+    "CardFace",
+    "CardDesign",
+]
